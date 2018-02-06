a0aa0e3f4174c54331b12f644fa2667af52a078e
Jinzhe Li <jinzhe@simple.com.au>
2018-02-06 11:03:12 +1030

    Enhance feature - make field content iteratable

--- src/models/LinkItContainer.php
+++ src/models/LinkItContainer.php
@@ -5,10 +5,15 @@ namespace simpleteam\linkit\models;
 
 use craft\base\Model;
 
-class LinkItContainer extends Model
+class LinkItContainer extends Model implements \IteratorAggregate
 {
     public $links = array();
     public function addLink(LinkItModel $link) {
         $this->links[] = $link;
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->links);
     }
 }
